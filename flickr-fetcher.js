d7aedc010cf49e494861dd02e9741af43dd03d87
fabiano taddei <fabiano.taddei@gmail.com>
2020-07-08 21:14:11 +0200

    part 1

---
+++ flickr-fetcher.js
@@ -0,0 +1,16 @@
+var FlickrFetcher;
+
+FlickrFetcher = {
+    photoObjToURL: function(photoObj) {
+        return `https://farm${photoObj.farm}.staticflickr.com/${photoObj.server}` +
+               `/${photoObj.id}_${photoObj.secret}_b.jpg`;
+    },
+    transformPhotoObj: function(photoObj) {
+       return {
+			title: photoObj.title,
+			url:   FlickrFetcher.photoObjToURL(photoObj)
+        };
+    }
+};
+
+module.exports = FlickrFetcher;
